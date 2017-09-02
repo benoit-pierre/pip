2b9acab4839a66ef0ac91f83e5e1902b963db444
Benoit Pierre <benoit.pierre@gmail.com>
2017-09-02 15:18:38 +0200

    travis: tweak number of jobs
    Go back to 8 jobs for non-PyPy builds.

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,10 +3,15 @@ set -e
 set -x
 
 if [[ $TOXENV == py* ]]; then
+    if [[ $TOXENV == pypy* ]]; then
+      jobs=6
+    else
+      jobs=8
+    fi
     # Run unit tests
     tox -- -m unit
     # Run integration tests
-    tox -- -m integration -n 4 --duration=5
+    tox -- -m integration -n $jobs --duration=5
 else
     # Run once
     tox
