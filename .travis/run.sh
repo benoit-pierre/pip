ed148f8f0f861bce74139de1915c49a6a15b59aa
Benoit Pierre <benoit.pierre@gmail.com>
2017-10-04 19:03:13 +0200

    tmp

--- .travis/run.sh
+++ .travis/run.sh
@@ -4,10 +4,10 @@ set -x
 
 if [[ $TOXENV == py* ]]; then
     # Run unit tests
-    tox -- -m unit
+    tox -- -m unit -n 3
     # Run integration tests
-    tox -- -m 'integration and not slow' -n 4 --duration=5
-    tox -- -m 'integration and slow' -n 2 --duration=5
+    tox -- -m 'integration and not slow' -n 3 --duration=5
+    tox -- -m 'integration and slow' -n 3 --duration=5
 else
     # Run once
     tox
