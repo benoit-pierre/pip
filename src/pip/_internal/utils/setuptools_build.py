eeecff71a2308cc8190d494416aacfded9d11b02
Benoit Pierre <benoit.pierre@gmail.com>
2017-10-04 15:12:53 +0200

    fix setuptools shim so `__requires__` is correctly handled

--- src/pip/_internal/utils/setuptools_build.py
+++ src/pip/_internal/utils/setuptools_build.py
@@ -1,8 +1,18 @@
 # Shim to wrap setup.py invocation with setuptools
 SETUPTOOLS_SHIM = (
-    "import setuptools, tokenize;__file__=%r;"
+    "import ast, itertools, tokenize;__file__=%r;"
     "f=getattr(tokenize, 'open', open)(__file__);"
     "code=f.read().replace('\\r\\n', '\\n');"
     "f.close();"
-    "exec(compile(code, __file__, 'exec'))"
+    "mod = ast.parse(code, __file__);"
+    "nodes = list(itertools.islice(("
+    "node for node in mod.body "
+    "if isinstance(node, ast.Assign) and "
+    "len(node.targets) == 1 and "
+    "isinstance(node.targets[0], ast.Name) and "
+    "node.targets[0].id == '__requires__'"
+    "), 0, 1));"
+    "__requires__ = ast.literal_eval(nodes[0].value) if nodes else [];"
+    "import setuptools;"
+    "exec(compile(mod, __file__, 'exec'));"
 )
