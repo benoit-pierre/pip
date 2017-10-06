b928b02033d0829d9897eacaec1f39cfd7057c39
Benoit Pierre <benoit.pierre@gmail.com>
2017-10-06 21:50:28 +0200

    fixup! tox: fix environment setup

--- .tox_helpers/runpip.py
+++ .tox_helpers/runpip.py
@@ -1,43 +1,26 @@
-from glob import glob
-import distutils.sysconfig
 import os
 import shutil
 import subprocess
 import sys
-import textwrap
 
 
-VIRTUAL_ENV = os.environ['VIRTUAL_ENV']
-TOX_PIP_DIR = os.path.join(VIRTUAL_ENV, 'pip')
-SITE_PACKAGES = distutils.sysconfig.get_python_lib()
+TOX_PIP_DIR = os.path.join(os.environ['VIRTUAL_ENV'], 'pip')
 
 
 def pip(args):
     # First things first, safeguard the environment
     # original pip so it can be used for all calls.
     if not os.path.exists(TOX_PIP_DIR):
-        os.mkdir(TOX_PIP_DIR)
-        # Remove executable/launchers.
-        for entry in glob(os.path.join(VIRTUAL_ENV, 'bin', 'pip*')):
-            os.unlink(entry)
-        # Relocate package and distribution info.
-        for src in (
-            os.path.join(SITE_PACKAGES, 'pip'),
-            glob(os.path.join(SITE_PACKAGES, 'pip-*.dist-info'))[0],
-        ):
-            shutil.move(src, TOX_PIP_DIR)
+        cmd = (
+            [sys.executable] +
+            '-m pip install -t'.split() +
+            [TOX_PIP_DIR, 'pip']
+        )
+        subprocess.check_call(cmd)
         # Create a very simple launcher that
         # can be used for Linux and Windows.
         with open(os.path.join(TOX_PIP_DIR, 'pip.py'), 'w') as fp:
-            fp.write(textwrap.dedent(
-                '''
-                import sys
-                from pip._vendor import pkg_resources
-                sys.exit(pkg_resources.load_entry_point(
-                    'pip', 'console_scripts', 'pip'
-                )())
-                '''
-            ).lstrip())
+            fp.write('from pip import main; main()')
     # And use a temporary copy of that version
     # so it can uninstall itself if needed.
     temp_pip = TOX_PIP_DIR + '.tmp'
