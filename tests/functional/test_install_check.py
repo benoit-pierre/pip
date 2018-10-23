456547197745943b8fbe2e487098933dca952f4b
Benoit Pierre <benoit.pierre@gmail.com>
2018-10-23 18:54:17 +0200

    tests: speedup `test_install_check` tests

--- tests/functional/test_install_check.py
+++ tests/functional/test_install_check.py
@@ -1,4 +1,4 @@
-from tests.lib import create_test_package_with_setup
+from tests.lib import create_basic_wheel_for_package
 
 
 def matches_expected_lines(string, expected_lines):
@@ -6,29 +6,25 @@ def matches_expected_lines(string, expected_lines):
 
 
 def test_check_install_canonicalization(script):
-    pkga_path = create_test_package_with_setup(
-        script,
-        name='pkgA',
-        version='1.0',
-        install_requires=['normal-missing', 'SPECIAL.missing'],
+    pkga_path = create_basic_wheel_for_package(
+        script.scratch_path, 'pkgA', '1.0',
+        depends=['normal-missing', 'SPECIAL.missing'],
     )
-    normal_path = create_test_package_with_setup(
-        script,
-        name='normal-missing', version='0.1',
+    normal_path = create_basic_wheel_for_package(
+        script.scratch_path, 'normal-missing', '0.1',
     )
-    special_path = create_test_package_with_setup(
-        script,
-        name='SPECIAL.missing', version='0.1',
+    special_path = create_basic_wheel_for_package(
+        script.scratch_path, 'SPECIAL.missing', '0.1',
     )
 
     # Let's install pkgA without its dependency
-    result = script.pip('install', '--no-index', pkga_path, '--no-deps')
+    result = script.pip_install_local(pkga_path, '--no-deps', links=None)
     assert "Successfully installed pkgA-1.0" in result.stdout, str(result)
 
     # Install the first missing dependency. Only an error for the
     # second dependency should remain.
-    result = script.pip(
-        'install', '--no-index', normal_path, '--quiet', expect_error=True
+    result = script.pip_install_local(
+        normal_path, '--quiet', expect_error=True, links=None,
     )
     expected_lines = [
         "pkga 1.0 requires SPECIAL.missing, which is not installed.",
@@ -39,8 +35,8 @@ def test_check_install_canonicalization(script):
     # Install the second missing package and expect that there is no warning
     # during the installation. This is special as the package name requires
     # name normalization (as in https://github.com/pypa/pip/issues/5134)
-    result = script.pip(
-        'install', '--no-index', special_path, '--quiet',
+    result = script.pip_install_local(
+        special_path, '--quiet', links=None,
     )
     assert matches_expected_lines(result.stderr, [])
     assert result.returncode == 0
@@ -55,30 +51,24 @@ def test_check_install_canonicalization(script):
 
 
 def test_check_install_does_not_warn_for_out_of_graph_issues(script):
-    pkg_broken_path = create_test_package_with_setup(
-        script,
-        name='broken',
-        version='1.0',
-        install_requires=['missing', 'conflict < 1.0'],
+    pkg_broken_path = create_basic_wheel_for_package(
+        script.scratch_path, 'broken', '1.0',
+        depends=['missing', 'conflict < 1.0'],
     )
-    pkg_unrelated_path = create_test_package_with_setup(
-        script,
-        name='unrelated',
-        version='1.0',
+    pkg_unrelated_path = create_basic_wheel_for_package(
+        script.scratch_path, 'unrelated', '1.0',
     )
-    pkg_conflict_path = create_test_package_with_setup(
-        script,
-        name='conflict',
-        version='1.0',
+    pkg_conflict_path = create_basic_wheel_for_package(
+        script.scratch_path, 'conflict', '1.0',
     )
 
     # Install a package without it's dependencies
-    result = script.pip('install', '--no-index', pkg_broken_path, '--no-deps')
+    result = script.pip_install_local(pkg_broken_path, '--no-deps', links=None)
     assert matches_expected_lines(result.stderr, [])
 
     # Install conflict package
-    result = script.pip(
-        'install', '--no-index', pkg_conflict_path, expect_error=True,
+    result = script.pip_install_local(
+        pkg_conflict_path, expect_error=True, links=None,
     )
     assert matches_expected_lines(result.stderr, [
         "broken 1.0 requires missing, which is not installed.",
@@ -89,8 +79,8 @@ def test_check_install_does_not_warn_for_out_of_graph_issues(script):
     ])
 
     # Install unrelated package
-    result = script.pip(
-        'install', '--no-index', pkg_unrelated_path, '--quiet',
+    result = script.pip_install_local(
+        pkg_unrelated_path, '--quiet', links=None,
     )
     # should not warn about broken's deps when installing unrelated package
     assert matches_expected_lines(result.stderr, [])
