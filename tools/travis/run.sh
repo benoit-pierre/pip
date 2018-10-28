2687dac99ed99f144fdaaaebae6604b07af8768c
Benoit Pierre <benoit.pierre@gmail.com>
2018-10-28 14:45:27 +0100

    tmp

--- tools/travis/run.sh
+++ tools/travis/run.sh
@@ -41,13 +41,15 @@ echo "TOXENV=${TOXENV}"
 set -x
 if [[ "$GROUP" == "1" ]]; then
     # Unit tests
-    tox -- -m unit
+    tox -- -m unit --durations=20
     # Integration tests (not the ones for 'pip install')
-    tox -- --use-venv -m integration -n 4 --duration=5 -k "not test_install"
+    # tox -- --use-venv -m integration -n 4 --duration=5 -k "not test_install"
 elif [[ "$GROUP" == "2" ]]; then
     # Separate Job for running integration tests for 'pip install'
-    tox -- --use-venv -m integration -n 4 --duration=5 -k "test_install"
+    # tox -- --use-venv -m integration -n 4 --duration=5 -k "test_install"
+    true
 else
     # Non-Testing Jobs should run once
-    tox
+    # tox
+    true
 fi
